7e0a00409a04b1b4d683a3beabd7717f6d32f657
Your Name <sakshikadam267@gmail.com>
2025-07-28 13:50:42 +0530

    added code in about_us

--- src/pages/About/AICTEApproval.jsx
+++ src/pages/About/AICTEApproval.jsx
@@ -1,2 +1,11 @@
-const Page = () => <div>AICTE Approval</div>;
-export default Page;
+import React from 'react'
+
+function AICTEApproval() {
+  return (
+    <div>
+      <h1>hiiii</h1>
+    </div>
+  )
+}
+
+export default AICTEApproval
